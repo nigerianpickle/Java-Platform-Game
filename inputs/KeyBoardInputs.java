4da5439e9bccd10b9791051e2a5513d880268ced
Daniel Nwogo <118936910+nigerianpickle@users.noreply.github.com>
2023-10-24 19:32:02 -0500

    Create KeyBoardInputs.java
    A package for all game inputs

---
+++ inputs/KeyBoardInputs.java
@@ -0,0 +1,24 @@
+package inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoardInputs implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("A key is pressedk");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+    
+}
